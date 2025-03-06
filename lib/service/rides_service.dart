589e179831aab1c74c5bafa53b2fbd5bf875b210
Sovansoupor <sovansouporeng@gmail.com>
2025-03-07 00:20:50 +0700

    BLA-104 – Test the RidesService on console

--- lib/service/rides_service.dart
+++ lib/service/rides_service.dart
@@ -1,3 +1,4 @@
+import 'package:week_3_blabla_project/repository/mock/mock_rides_repository.dart';
 import 'package:week_3_blabla_project/repository/rides_repository.dart';
 
 import '../dummy_data/dummy_data.dart';
@@ -12,37 +13,32 @@ class RidesService {
   static List<Ride> availableRides = fakeRides;
 
   // Singleton instance of RidesService (ensures only one instance exists)
-  static final RidesService _instance = RidesService._internal();
-
-  // Late-initialized repository for fetching rides (must be assigned before use)
-  static late RideRepository _repository;
+  static RidesService? _instance;
+  final RideRepository repository;
 
   // Private named constructor (prevents direct instantiation from outside the class)
   // ensuring only one instance is used
-  RidesService._internal();
+  RidesService._internal(this.repository);
+
+
+  ///
+  /// Initialize the service with a RideRepository implementation.
+  ///
+  static void initialize(RideRepository repository) {
+    _instance ??= RidesService._internal(repository);
+  }
+
+  static RidesService get instance{
+    _instance ??= RidesService._internal(MockRidesRepository());
+    return _instance!;
+  }
 
   // Fetches a list of rides based on user preferences and an optional filter.
   // Calls the `_repository` to retrieve data.
   List<Ride> getRides(RidePreference preference, RidesFilter? filter) {
-    return _repository.getRides(preference, filter);
+    return repository.getRides(preference, filter);
   }
-
-  ///
-  /// Initialize the service with a RideRepository implementation.
-  /// This method must be called before using `getRides()`, as `_repository` is late-initialized.
-  ///
-  static void initialize(RideRepository repository) {
-    _repository = repository;
-  }
-
-  ///
-  /// Factory constructor to return the singleton instance of RidesService.
-  /// Ensures only one instance is used throughout the application.
-  ///
-  factory RidesService() {
-    return _instance;
-  }
-
+  
   ///
   /// Returns a list of rides that match the given user preferences.
   /// This method filters rides based on departure and arrival locations.
