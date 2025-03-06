145f5853fbc94bdb2457bac831a86da2c1087e94
Sovansoupor <sovansouporeng@gmail.com>
2025-03-06 20:10:09 +0700

    BLA-101 – Refactor Location Service

--- lib/widgets/inputs/bla_location_picker.dart
+++ lib/widgets/inputs/bla_location_picker.dart
@@ -8,8 +8,7 @@ import '../../theme/theme.dart';
 /// This full-screen modal is in charge of providing (if confirmed) a selected location.
 ///
 class BlaLocationPicker extends StatefulWidget {
-  final Location?
-      initLocation; // The picker can be triguer with an existing location name
+  final Location? initLocation; // The picker can be triguer with an existing location name
 
   const BlaLocationPicker({super.key, this.initLocation});
 
@@ -60,8 +59,8 @@ class _BlaLocationPickerState extends State<BlaLocationPicker> {
         .where((location) =>
             location.name.toUpperCase().contains(text.toUpperCase()))
         .toList();
-      
   }
+
 
   @override
   Widget build(BuildContext context) {
