a92f4c84b63ad6082cfe7c5b6124b60107cbac55
Sovansoupor <sovansouporeng@gmail.com>
2025-03-07 00:20:26 +0700

    BLA-104 – Test the RidesService on console

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:week_3_blabla_project/repository/locations_repository.dart';
 import 'package:week_3_blabla_project/repository/mock/mock_locations_repository.dart';
 import 'package:week_3_blabla_project/repository/mock/mock_ride_preferences_repository.dart';
+import 'package:week_3_blabla_project/repository/mock/mock_rides_repository.dart';
 import 'package:week_3_blabla_project/service/locations_service.dart';
+import 'package:week_3_blabla_project/service/rides_service.dart';
 
 
 
@@ -15,6 +16,7 @@ void main() {
   // 1 - Initialize the services
   RidePrefService.initialize(MockRidePreferencesRepository()); // Initialize the ride preferences service
   LocationsService.initialize(MockLocationsRepository()); // Initialize the locations service
+  RidesService.initialize(MockRidesRepository());
 
    //2- Use the controller
    
